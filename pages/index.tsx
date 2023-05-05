76fb7fb298b4ea74d6fda02a0a9e9dff4143b176
Stephen Tewes <stephentewes36@gmail.com>
2023-05-05 16:04:54 -0400

    Create Navbar and Loader

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Loader from "../components/Loader";
 
 export default function Home() {
     return (
         <div>
-            <Link href="/stephen2s">
-                <a>Stephen's profile</a>
-            </Link>
+            <Loader show />
         </div>
     );
 }
